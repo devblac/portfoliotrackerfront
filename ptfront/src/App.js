1473a4ad060c27b1db744ae3b39007813d02e77b
Julian Blacher <julian.blacher@gmail.com>
2022-04-01 10:59:59 -0300

    adding Lifecycle component

--- ptfront/src/App.js
+++ ptfront/src/App.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import BasicButtonGroup from './components/menu';
 import Lifecycle from './Lifecycle';
 import FetchCard from './FetchCard';
+import ResizeApp from './ResizeApp';
+import React from 'react';
 
 function App() {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="App">
-      {/* <Lifecycle /> */}
-      <FetchCard />
+      <button onClick={()=> setShow(!show)} >
+        Show/Hide 
+      </button>
+      { show && <Lifecycle /> }
+      
+      {/* <FetchCard /> */}
+      {/* {show && <ResizeApp />} */}
     </div>
   );
 }
