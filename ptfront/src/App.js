1eea22d48fa9a0772c6b2896b5175491132ce83d
Julian Blacher <julian.blacher@gmail.com>
2022-04-02 13:18:34 -0300

    test wip

--- ptfront/src/App.js
+++ ptfront/src/App.js
@@ -5,19 +5,26 @@ import Lifecycle from './Lifecycle';
 import FetchCard from './FetchCard';
 import ResizeApp from './ResizeApp';
 import React from 'react';
+import { CounterApp } from './CounterApp';
+/* import TodoApp from './TodoApp'; */
+import MouseApp from './MouseApp';
 
 function App() {
   const [show, setShow] = useState(false);
 
   return (
     <div className="App">
-      <button onClick={()=> setShow(!show)} >
+      {/* <button onClick={()=> setShow(!show)} >
         Show/Hide 
       </button>
-      { show && <Lifecycle /> }
+      { show && <Lifecycle /> } */}
       
       {/* <FetchCard /> */}
       {/* {show && <ResizeApp />} */}
+
+      {/* <CounterApp /> */}
+      {/* <TodoApp /> */}
+      <MouseApp />
     </div>
   );
 }
