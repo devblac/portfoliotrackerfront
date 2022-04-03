bf84ea97871bad36b6ec36291cf272d138c834ae
Julian Blacher <julian.blacher@gmail.com>
2022-04-03 19:30:05 -0300

    wip

--- ptfront/src/App.js
+++ ptfront/src/App.js
@@ -1,30 +1,13 @@
 import { useState } from 'react';
 import './App.css';
 import BasicButtonGroup from './components/menu';
-import Lifecycle from './Lifecycle';
-import FetchCard from './FetchCard';
-import ResizeApp from './ResizeApp';
 import React from 'react';
-import { CounterApp } from './CounterApp';
-/* import TodoApp from './TodoApp'; */
-import MouseApp from './MouseApp';
 
 function App() {
-  const [show, setShow] = useState(false);
 
   return (
     <div className="App">
-      {/* <button onClick={()=> setShow(!show)} >
-        Show/Hide 
-      </button>
-      { show && <Lifecycle /> } */}
-      
-      {/* <FetchCard /> */}
-      {/* {show && <ResizeApp />} */}
-
-      {/* <CounterApp /> */}
-      {/* <TodoApp /> */}
-      <MouseApp />
+      <h1>Hi</h1>
     </div>
   );
 }
